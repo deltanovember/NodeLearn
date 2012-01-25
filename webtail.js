5de5d04fd8dd843f351b8eb04b0e47710fddbe3c
Don Nguyen <don@unswrc.com>
2012-01-26 09:59:49 +1100

    First commit

---
+++ webtail.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var spawn = require('child_process').spawn;
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        'Content-Type':'text/plain'
+    });
+
+    var tail_child = spawn('tail', ['-f', 'test.Q']);
+    tail_child.stdout.on('data', function(data) {
+       console.log(data.toString());
+        response.write(data);
+
+    });
+}).listen(9000);
